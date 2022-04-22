e6f9365115dac632955c3447c1b0c0753b91aa03
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-04-22 16:41:44 +0200

    update reservationModal file

--- src/modules/reservationModal.js
+++ src/modules/reservationModal.js
@@ -1,54 +1,54 @@
 import TVShowApp from "../App";
 
-const reservationModal = async (show) =>{
-    // modal container
-    const modalContainer = document.getElementById('reservation-popUp');
+const reservationModal = async (show) => {
+  // modal container
+  const modalContainer = document.getElementById('reservation-popUp');
 
-    // boxshadow child of modal container
-    const backshadow = document.createElement('div');
-    backshadow.className = 'backshadow';
+  // boxshadow child of modal container
+  const backshadow = document.createElement('div');
+  backshadow.className = 'backshadow';
 
-    // modal child of backshadow
-    const modal = document.createElement('div');
-    modal.className = 'modal';
+  // modal child of backshadow
+  const modal = document.createElement('div');
+  modal.className = 'modal';
 
-    // child of modal
-    const imgDiv = document.createElement('div');
-    imgDiv.className = 'img-div';
+  // child of modal
+  const imgDiv = document.createElement('div');
+  imgDiv.className = 'img-div';
 
-    // child of imgDiv
-    const commentModalImg = document.createElement('img');
-    commentModalImg.src = show.image;
-    commentModalImg.setAttribute('alt',"moview-image");
-    imgDiv.appendChild(commentModalImg);
+  // child of imgDiv
+  const commentModalImg = document.createElement('img');
+  commentModalImg.src = show.image;
+  commentModalImg.setAttribute('alt', "moview-image");
+  imgDiv.appendChild(commentModalImg);
 
-    // child of modal
-    const title = document.createElement('h3');
-    title.textContent = show.title;
-    title.className = 'movie-title';
+  // child of modal
+  const title = document.createElement('h3');
+  title.textContent = show.title;
+  title.className = 'movie-title';
 
-    // child of modal
-    const details = document.createElement('div');
-    details.className = 'movie-details';
+  // child of modal
+  const details = document.createElement('div');
+  details.className = 'movie-details';
 
-    // children of details
-    const genre = document.createElement('span');
-    genre.textContent = show.genres[0];
+  // children of details
+  const genre = document.createElement('span');
+  genre.textContent = show.genres[0];
 
-    const language = document.createElement('span');
-    language.textContent = show.language;
+  const language = document.createElement('span');
+  language.textContent = show.language;
 
-    // details appending its children
-    details.appendChild(genre);
-    details.appendChild(language);
+  // details appending its children
+  details.appendChild(genre);
+  details.appendChild(language);
 
-    // child of modal
-    const cross = document.createElement('i');
-    cross.className = 'fa-solid fa-xmark fa-xl cross';
+  // child of modal
+  const cross = document.createElement('i');
+  cross.className = 'fa-solid fa-xmark fa-xl cross';
 
-    cross.addEventListener('click',()=>{
-      backshadow.style.display = 'none';
-    })
+  cross.addEventListener('click', () => {
+    backshadow.style.display = 'none';
+  })
 
 
   //-------------
@@ -57,20 +57,20 @@ const reservationModal = async (show) =>{
   reservationtForm.className = 'form-group comment-form';
 
   const nameInput = document.createElement('input');
-  nameInput.type ='text';
-  nameInput.className='form-control custom-inputs userName';
+  nameInput.type = 'text';
+  nameInput.className = 'form-control custom-inputs userName';
   nameInput.placeholder = 'Enter your name';
   reservationtForm.appendChild(nameInput);
 
   const startDateInput = document.createElement('input');
-  startDateInput.type ='date';
-  startDateInput.className='form-control custom-inputs startDate';
+  startDateInput.type = 'date';
+  startDateInput.className = 'form-control custom-inputs startDate';
   startDateInput.placeholder = 'Start date';
   reservationtForm.appendChild(startDateInput);
 
   const endDateInput = document.createElement('input');
-  endDateInput.type ='date';
-  endDateInput.className='form-control custom-inputs endDate';
+  endDateInput.type = 'date';
+  endDateInput.className = 'form-control custom-inputs endDate';
   endDateInput.placeholder = 'End date';
   reservationtForm.appendChild(endDateInput);
 
@@ -81,70 +81,106 @@ const reservationModal = async (show) =>{
 
   //---------- Display reservation
 
-    const response = await TVShowApp.getReservations(show.id);
-    const reservationList = document.createElement('ul');
-    console.log(response);
-    if (response.length > 0) {
-      response.forEach(reservation => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `From: ${reservation.date_start} To: ${reservation.date_end} UserName: ${reservation.username}`
-        reservationList.appendChild(listItem);
-      });
-    }else{
+  const response = await TVShowApp.getReservations(show.id);
+  const reservationList = document.createElement('ul');
+  console.log(response);
+  if (response.length > 0) {
+    response.forEach(reservation => {
       const listItem = document.createElement('li');
-        listItem.textContent = `No Reservations exist for this show`
-        reservationList.appendChild(listItem);
-    }
+      listItem.textContent = `From: ${reservation.date_start} To: ${reservation.date_end} UserName: ${reservation.username}`
+      reservationList.appendChild(listItem);
+    });
+  } else {
+    const listItem = document.createElement('li');
+    listItem.textContent = `No Reservations exist for this show`
+    reservationList.appendChild(listItem);
+  }
 
-    reservationtForm.addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const myUserName = document.querySelector('.userName');
-      const myStartDate = document.querySelector('.startDate');
-      const myEndDate = document.querySelector('.endDate');
-    
-        const ReserveResponse =  await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/bPjiahSZ0zVQqd4kdfjM/reservations?item_id=${show.id}`, {
-          method: 'POST',
-          body: JSON.stringify({
-            item_id : show.id,
-            username : myUserName.value.trim(),
-            date_start : myStartDate.value.trim(),
-            date_end : myEndDate.value.trim()
-          }),
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-      })
+  reservationtForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const myUserName = document.querySelector('.userName');
+    const myStartDate = document.querySelector('.startDate');
+    const myEndDate = document.querySelector('.endDate');
+
+    const ReserveResponse = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/bPjiahSZ0zVQqd4kdfjM/reservations?item_id=${show.id}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: show.id,
+        username: myUserName.value.trim(),
+        date_start: myStartDate.value.trim(),
+        date_end: myEndDate.value.trim()
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
       .then(response => response)
       .then(() => {
         myUserName.value = '';
         myStartDate.value = '';
-        myEndDate.value = '' 
-      }) 
+        myEndDate.value = ''
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            const response = JSON.parse(this.responseText);
+            if (response.length > 0) {    
+              // reservationList = document.createElement('ul');
+              response.forEach(reservation => {
+                const listItem = document.createElement('li');
+                listItem.textContent = `From: ${reservation.date_start} To: ${reservation.date_end} UserName: ${reservation.username}`
+                reservationList.appendChild(listItem);
+              });
+            } 
+            else {
+              const listItem = document.createElement('li');
+              listItem.textContent = `No Reservations exist for this show`
+              reservationList.appendChild(listItem);
+            }
+            reservationHeading = document.createElement('h5');
+            reservationHeading.textContent = `Reservations(${response.length})`;
+            reservationHeading.className = 'comments-heading';
+
+            modal.innerHTML = "";
+
+            modal.appendChild(imgDiv);
+            modal.appendChild(title);
+            modal.appendChild(details);
+            modal.appendChild(cross);
+            modal.appendChild(reservationHeading);
+            modal.appendChild(reservationList);
+            modal.appendChild(reservationtForm);
+
+          }
+        };
+        xhttp.open("GET", `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/bPjiahSZ0zVQqd4kdfjM/reservations?item_id=${show.id}`, true);
+        xhttp.send();
       })
-    
-    
-    // child of modal
-    const reservationHeading = document.createElement('h5');
-    reservationHeading.textContent = `Reservations(${response.length})`;
-    reservationHeading.className = 'comments-heading';
+  })
 
-    // child of modal
-    
 
-    // modal appending its children
-    modal.appendChild(imgDiv);
-    modal.appendChild(title);
-    modal.appendChild(details);
-    modal.appendChild(cross);
-    modal.appendChild(reservationHeading);
-    modal.appendChild(reservationList);
-    modal.appendChild(reservationtForm);
+  // child of modal
+  const reservationHeading = document.createElement('h5');
+  reservationHeading.textContent = `Reservations(${response.length})`;
+  reservationHeading.className = 'comments-heading';
 
-    // backshadow appending modal
-    backshadow.appendChild(modal);
+  // child of modal
 
-    // modal container appending backshadow
-    modalContainer.appendChild(backshadow);
+
+  // modal appending its children
+  modal.appendChild(imgDiv);
+  modal.appendChild(title);
+  modal.appendChild(details);
+  modal.appendChild(cross);
+  modal.appendChild(reservationHeading);
+  modal.appendChild(reservationList);
+  modal.appendChild(reservationtForm);
+
+  // backshadow appending modal
+  backshadow.appendChild(modal);
+
+  // modal container appending backshadow
+  modalContainer.appendChild(backshadow);
 }
 
 export default reservationModal;
